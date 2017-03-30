12743af4c298777309cadf16d442f0443e1420d9
He Zhang (Rudy) <425220727@qq.com>
2017-03-30 21:24:40 +0800

    Update 02_05_AddTwoNums.java

--- chapter_02/02_05_AddTwoNums.java
+++ chapter_02/02_05_AddTwoNums.java
@@ -46,6 +46,21 @@ public class AddTwoNums {
 		}
 	}
 	
+	public ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		ListNode p1 = head;
+		ListNode p2 = p1.next;
+		while (p2 != null) {
+			ListNode p3 = p2.next;
+			p2.next = p1;
+			p1 = p2;
+			p2 = p3;
+		}
+		head.next = null;
+		return p1;
+	}
+	
 	public static void main(String[] args) {
 		
 
