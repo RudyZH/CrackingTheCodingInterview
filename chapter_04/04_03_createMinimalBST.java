a5efb4274bb57748c595c65b454e0b5e3f642d87
He Zhang (Rudy) <425220727@qq.com>
2017-04-11 22:14:12 +0800

    Create 04_03_createMinimalBST.java

---
+++ chapter_04/04_03_createMinimalBST.java
@@ -0,0 +1,12 @@
+TreeNode createMinimalBST(int[] arr, int start, int end) {
+    if (end < start) return null;
+    int mid = (start + end) / 2;
+    TreeNode n = new TreeNode(arr[mid]);
+    n.left = createMinimalBST(arr, start, mid - 1);
+    n.right = createMinimalBST(arr, mid + 1, end);
+    return n;
+}
+
+TreeNode createMinimalBST(int[] arr) {
+    return createMinimalBST(array, 0, array.length - 1);
+}
